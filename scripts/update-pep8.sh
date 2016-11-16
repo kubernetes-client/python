273684c60102440b0a6b29beff9d17a61959154e
mbohlool <mehdy@google.com>
2016-11-16 15:41:00 -0800

    Add semi-auto code formatting script

---
+++ scripts/update-pep8.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+# Copyright 2015 The Kubernetes Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Script to fetch latest swagger spec.
+# Puts the updated spec at api/swagger-spec/
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+if ! which virtualenv > /dev/null 2>&1; then
+  echo "virtualenv is not installed. run: [sudo] pip install virtualenv"
+  exit
+fi
+
+SCRIPT_ROOT=$(dirname "${BASH_SOURCE}")
+CLIENT_ROOT="${SCRIPT_ROOT}/.."
+
+pushd "${SCRIPT_ROOT}" > /dev/null
+SCRIPT_ROOT=`pwd`
+popd > /dev/null
+
+pushd "${CLIENT_ROOT}" > /dev/null
+CLIENT_ROOT=`pwd`
+popd > /dev/null
+
+virtualenv "${SCRIPT_ROOT}/.py"
+
+VIRTUAL_ENV_DISABLE_PROMPT=1; source "${SCRIPT_ROOT}/.py/bin/activate"
+trap "deactivate" EXIT SIGINT
+
+SAVEIFS=$IFS
+trap "IFS=$SAVEIFS" EXIT SIGINT
+IFS=,
+
+SOURCES="${CLIENT_ROOT}/k8sutil/*.py,${SCRIPT_ROOT}/*.py,${CLIENT_ROOT}/examples/*.py"
+
+echo "--- Updating tools"
+pip install --upgrade pep8
+pip install --upgrade autopep8
+pip install --upgrade isort
+
+echo "--- applying autopep8"
+for SOURCE in $SOURCES; do
+    autopep8 -i -a -a $SOURCE
+done
+
+echo "--- applying isort"
+for SOURCE in $SOURCES; do
+    isort -y $SOURCE
+done
+
+echo "--- check pep8 (all need to be fixed manually)"
+set +o errexit
+for SOURCE in $SOURCES; do
+    pep8 $SOURCE
+done
+
+echo "---Done."
