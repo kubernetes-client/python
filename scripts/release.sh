610a1f5794f10e4d2b9b0b16feb96e37e947c7da
mbohlool <mehdy@google.com>
2016-12-27 17:13:17 -0800

    document release steps in a bash file.

---
+++ scripts/release.sh
@@ -0,0 +1,9 @@
+# This file is intended to document release steps.
+# Verify each step's result before calling the next command.
+# It is documented here with the intention of being automated
+# as a shell script later.
+
+echo 'git clean -xdf'
+echo 'python setup.py bdist_wheel --universal'
+echo 'twine upload dist/* -r https://upload.pypi.org/legacy/ -u kubernetes'
+
