68e6fd2bbc48c0a5f14d754ead39af6a63f15495
mbohlool <mehdy@google.com>
2017-01-03 14:09:12 -0800

    Update release steps to add source distribution

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,6 +4,7 @@
 # as a shell script later.
 
 echo 'git clean -xdf'
+echo 'python setup.py sdist'
 echo 'python setup.py bdist_wheel --universal'
 echo 'twine upload dist/* -r https://upload.pypi.org/legacy/ -u kubernetes'
 
