61aad240acd3e0ca6fdf52e84e980a13a34b0fd3
mbohlool <mehdy@google.com>
2017-01-10 13:33:56 -0800

    Update release version to 1.0.0a5

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import find_packages, setup
 
 # Do not edit these constants. They will be updated automatically
 # by scripts/update-client.sh.
-CLIENT_VERSION = "1.0.0a4"
+CLIENT_VERSION = "1.0.0a5"
 PACKAGE_NAME = "kubernetes"
 DEVELOPMENT_STATUS = "3 - Alpha"
 
