2fcae28aabb7344223f8322f05dd4d45ce999372
mbohlool <mehdy@google.com>
2017-02-21 14:09:31 -0800

    Update version constants for X1.0.0b2 release

--- scripts/constants.py
+++ scripts/constants.py
@@ -23,7 +23,7 @@ SPEC_VERSION = "v1.5.1"
 
 # client version for packaging and releasing. It can
 # be different than SPEC_VERSION.
-CLIENT_VERSION = "1.0.0b1"
+CLIENT_VERSION = "1.0.0b2"
 
 # Name of the release package
 PACKAGE_NAME = "kubernetes"
