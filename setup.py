7d558a41e3c7a63bcfd5b71464bb2b5fd0333693
mbohlool <mehdy@google.com>
2017-02-22 14:42:01 -0800

    Update version constants for X1.0.0b3 release

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import find_packages, setup
 
 # Do not edit these constants. They will be updated automatically
 # by scripts/update-client.sh.
-CLIENT_VERSION = "1.0.0b2"
+CLIENT_VERSION = "1.0.0b3"
 PACKAGE_NAME = "kubernetes"
 DEVELOPMENT_STATUS = "4 - Beta"
 
