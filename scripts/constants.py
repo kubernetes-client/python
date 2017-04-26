78790ac0f20d031eaca14dae80086447587bfc94
mbohlool <mehdy@google.com>
2017-04-26 14:08:36 -0700

    Release 1.0.1

--- scripts/constants.py
+++ scripts/constants.py
@@ -23,7 +23,7 @@ SPEC_VERSION = "v1.5.3"
 
 # client version for packaging and releasing. It can
 # be different than SPEC_VERSION.
-CLIENT_VERSION = "1.0.0"
+CLIENT_VERSION = "1.0.1"
 
 # Name of the release package
 PACKAGE_NAME = "kubernetes"
