b7668d5e44711b5e6661e9ba04e1118af9716cd0
Jonathan Kosgei <jonathankosgei1000@gmail.com>
2017-05-11 08:21:46 +0300

    fix example and tags - naming and pep

--- examples/create_thirdparty_resource.py
+++ examples/create_thirdparty_resource.py
@@ -1,27 +1,37 @@
 from __future__ import print_function
-import time
-import kube_resource
-import json
-from kube_resource.rest import ApiException
+
+import kubernetes
+
+from kubernetes.rest import ApiException
+from kubernetes import config
 from pprint import pprint
 
 with open('/var/run/secrets/kubernetes.io/serviceaccount/token', 'r') as token_file:
     token=token_file.read()
 
-kube_resource.configuration.api_key['Authorization'] = token
-kube_resource.configuration.api_key_prefix['Authorization'] = 'Bearer'
-kube_resource.configuration.ssl_ca_cert = '/var/run/secrets/kubernetes.io/serviceaccount/ca.crt'
-api_instance = kube_resource.DefaultApi()
+config.load_kube_config()
+api_instance = kubernetes.ThirdPartyResources()
 
 namespace = 'default'
 resource = 'repos'
 fqdn = 'git.k8s.com'
 
-body = json.loads('{  "apiVersion": "git.k8s.com/v1",  "kind": "RePo",  "metadata": {    "name": "blog-repo"  },  "repo": "github.com/user/my-blog",  "oauth": 123456789,  "branch": "master",  "key": "user-git-deploy-secret",  "path": "/path/in/volume",  "image": "image to run job in",  "then": "hugo --destination=/home/user/hugosite/public",  "pvc": "persistent-volume-claim",  "username": "repo username",  "password": "repo password"}')
+body = {}
+body['apiVersion'] = "git.k8s.com/v1"
+body['kind'] = "RePo"
+body['metadata'] = {}
+body['metadata']['name'] = "blog-repo"
+body['repo'] = "github.com/user/my-blog"
+body['username'] = "username"
+body['password'] = "password"
+body['branch'] = "branch"
 
 try: 
     # Create a Resource
-    api_response = api_instance.apis_fqdn_v1_namespaces_namespace_resource_post(namespace, fqdn, resource, body)
+    api_response = api_instance.apis_fqdn_v1_namespaces_namespace_resource_post(
+        namespace, fqdn, resource, body)
     pprint(api_response)
 except ApiException as e:
-    print("Exception when calling DefaultApi->apis_fqdn_v1_namespaces_namespace_resource_post: %s\n" % e)
+    print(
+        "Exception when calling DefaultApi->apis_fqdn_v1_namespaces_namespace_resource_post: %s\n" % 
+        e)
