1054f10c6d526a52c3b3a9756a231d2c923bbaf8
Jochen Breuer <jbreuer@suse.de>
2017-08-10 23:24:56 +0200

    Introducing __version__ and __project__ variables
    Version information comes from scripts/constants.py and is hard-coded to
    kubernetes/__init__py by scripts/update-client.sh.
    
    __project__ is just hard-coded.
    
    Fixes #317

--- kubernetes/__init__.py
+++ kubernetes/__init__.py
@@ -12,6 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+__project__ = 'kubernetes'
+# The version is auto-updated. Please do not edit.
+__version__ = None
+
 import kubernetes.client
 import kubernetes.config
 import kubernetes.watch
