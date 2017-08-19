d457307a22849212673c79d05a0eb6e97e41a2b8
Tomasz Prus <tomasz.prus@gmail.com>
2017-08-19 02:21:05 +0200

    fix: pep8

--- examples/async.py
+++ examples/async.py
@@ -1,5 +1,6 @@
 import asyncio
 import functools
+
 from kubernetes import client, config
 
 
@@ -8,12 +9,14 @@ def create_async_callback():
     def set_result(loop, future, data):
         loop.call_soon_threadsafe(future.set_result, data)
     future = asyncio.Future()
-    return functools.partial(set_result, asyncio.get_event_loop(), future), future
+    return functools.partial(
+        set_result, asyncio.get_event_loop(), future), future
 
 
 async def main():
 
-    # Configs can be set in Configuration class directly or using helper utility
+    # Configs can be set in Configuration class directly or using helper
+    # utility
     config.load_kube_config()
 
     v1 = client.CoreV1Api()
@@ -23,7 +26,11 @@ async def main():
     v1.list_pod_for_all_namespaces(watch=False, callback=callback)
     ret = await future
     for i in ret.items:
-        print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+        print(
+            "%s\t%s\t%s" %
+            (i.status.pod_ip,
+             i.metadata.namespace,
+             i.metadata.name))
 
 
 if __name__ == "__main__":
--- examples/callbacks.py
+++ examples/callbacks.py
@@ -1,11 +1,16 @@
 import time
+
 from kubernetes import client, config
 
 
 def recv_namespace(data):
     print("Listing pods with their IPs:")
     for i in data.items:
-        print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+        print(
+            "%s\t%s\t%s" %
+            (i.status.pod_ip,
+             i.metadata.namespace,
+             i.metadata.name))
 
 
 # Configs can be set in Configuration class directly or using helper utility
