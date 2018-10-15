7c0220af9517b3ac7c402cdc09d0313e1f669b58
Haowei Cai <haoweic@google.com>
2018-10-15 16:21:21 -0700

    generated client change for 8.0.0b1 release

--- kubernetes/client/api_client.py
+++ kubernetes/client/api_client.py
@@ -71,7 +71,7 @@ class ApiClient(object):
             self.default_headers[header_name] = header_value
         self.cookie = cookie
         # Set default User-Agent.
-        self.user_agent = 'Swagger-Codegen/8.0.0a1/python'
+        self.user_agent = 'Swagger-Codegen/8.0.0b1/python'
     
     def __del__(self):
         self.pool.close()
