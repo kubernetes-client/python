bb0eba05506c759ffdcce27bc308db34418beb93
Haowei Cai <haoweic@google.com>
2018-12-27 17:05:04 -0800

    Update version constant for 7.0.1 release

--- scripts/constants.py
+++ scripts/constants.py
@@ -18,7 +18,7 @@ import sys
 KUBERNETES_BRANCH = "release-1.11"
 
 # client version for packaging and releasing.
-CLIENT_VERSION = "7.0.0"
+CLIENT_VERSION = "7.0.1"
 
 # Name of the release package
 PACKAGE_NAME = "kubernetes"
