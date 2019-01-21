4a3c232a5c2a97b71070f240f5599e0b601ea509
Sai Adarsh <sivakumar.sk11865.sk@gmail.com>
2019-01-21 15:41:37 -0500

    Fixes Setup,py error on Windows #676
    Fixes Setup,py error on Windows, added shell script to check python environment, print the python version, copy files from /kubernetes/base to /kubernetes/ and finally runs python setup.py install

---
+++ scripts/setup-fix.bat
@@ -0,0 +1,11 @@
+if ! [ -x "$(command -v python)" ];
+then 
+	echo Python environment notfound..
+else 
+	echo Python environment found..
+fi
+python --version
+cp -R ../kubernetes/base/* ../kubernetes/
+cd ../ && python setup.py install
+
+
