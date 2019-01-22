769ef0ea65925fc671862452a581ea6f57f78b06
Sai Adarsh <sivakumar.sk11865.sk@gmail.com>
2019-01-23 00:26:22 +0530

    Update setup-fix.bat

--- scripts/setup-fix.bat
+++ scripts/setup-fix.bat
@@ -1,8 +1,8 @@
 if ! [ -x "$(command -v python)" ];
 then 
-	echo Python environment notfound..
+	echo "Python environment notfound.."
 else 
-	echo Python environment found..
+	echo "Python environment found.."
 fi
 python --version
 cp -R ../kubernetes/base/* ../kubernetes/
