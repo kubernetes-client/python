357f23e42a64cdc9c3bc177b886944288bd5c195
Sai Adarsh <sivakumar.sk11865.sk@gmail.com>
2019-01-25 01:18:50 +0530

    Update setup-fix.bat

--- scripts/setup-fix.bat
+++ scripts/setup-fix.bat
@@ -1,5 +1,8 @@
-( 	where python >null
-	echo Python environment found..
+( 	python --version>nul 2>&1 && (
+		echo Python environment found..
+	) || (
+	    echo Python environment not found..
+	)
 	python --version
 	copy ..\kubernetes\base\* ..\kubernetes\ 
 	cd ../ && python setup.py install )
