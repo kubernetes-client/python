3fb317f2ea446abbb306c0068cbc82a79095a0f9
Sai Adarsh <sivakumar.sk11865.sk@gmail.com>
2019-01-29 11:25:08 +0530

    Update windows-setup-fix.bat

--- scripts/windows-setup-fix.bat
+++ scripts/windows-setup-fix.bat
@@ -1,5 +1,5 @@
 (	python --version>nul 2>&1 && ( REM checks the availability of python environment
-	echo Python environment found. REM prints if python environment is found
+	echo "Python environment found." REM prints if python environment is found
 	)
 	python --version REM prints python environment version
 	copy ..\kubernetes\base\* ..\kubernetes\ REM copies every file and folder that's inside \kubernetes\base\ to \kubernetes\	
