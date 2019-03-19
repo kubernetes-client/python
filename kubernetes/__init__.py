23e4df62e98318745a3c86590a8e2b0508d58dec
Haowei Cai <haoweic@google.com>
2019-03-18 18:32:27 -0700

    generated client change for 9.0.0 release

--- kubernetes/__init__.py
+++ kubernetes/__init__.py
@@ -14,7 +14,7 @@
 
 __project__ = 'kubernetes'
 # The version is auto-updated. Please do not edit.
-__version__ = "9.0.0b1"
+__version__ = "9.0.0"
 
 import kubernetes.client
 import kubernetes.config
