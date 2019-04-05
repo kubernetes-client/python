18f45c9ceca522d1202cb2b5b7c98f32321fe516
Oz Tiram <oz.tiram@gmail.com>
2019-04-05 06:30:43 +0200

    Fix wrong import name

--- kubernetes/utils/__init__.py
+++ kubernetes/utils/__init__.py
@@ -15,4 +15,4 @@
 from __future__ import absolute_import
 
 from .create_from_yaml import (FailToCreateError, create_from_yaml,
-                               create_from_map)
+                               create_from_dict)
