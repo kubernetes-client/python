51db2d66fbd06daaddc6e8038c709b2727a6a059
Mariusz Mazur <mmazur@redhat.com>
2019-06-05 11:38:46 +0200

    Correct wording

--- kubernetes/client/models/v1_pod_affinity.py
+++ kubernetes/client/models/v1_pod_affinity.py
@@ -58,7 +58,7 @@ class V1PodAffinity(object):
     def preferred_during_scheduling_ignored_during_execution(self):
         """
         Gets the preferred_during_scheduling_ignored_during_execution of this V1PodAffinity.
-        The scheduler will prefer to schedule pods to nodes that satisfy the affinity expressions specified by this field, but it may choose a node that violates one or more of the expressions. The node that is most preferred is the one with the greatest sum of weights, i.e. for each node that meets all of the scheduling requirements (resource request, requiredDuringScheduling affinity expressions, etc.), compute a sum by iterating through the elements of this field and adding \"weight\" to the sum if the node has pods which matches the corresponding podAffinityTerm; the node(s) with the highest sum are the most preferred.
+        The scheduler will prefer to schedule pods to nodes that satisfy the affinity expressions specified by this field, but it may choose a node that violates one or more of the expressions. The node that is most preferred is the one with the greatest sum of weights, i.e. for each node that meets all of the scheduling requirements (resource request, requiredDuringScheduling affinity expressions, etc.), compute a sum by iterating through the elements of this field and adding \"weight\" to the sum if the node has pods which match the corresponding podAffinityTerm; the node(s) with the highest sum are the most preferred.
 
         :return: The preferred_during_scheduling_ignored_during_execution of this V1PodAffinity.
         :rtype: list[V1WeightedPodAffinityTerm]
@@ -69,7 +69,7 @@ class V1PodAffinity(object):
     def preferred_during_scheduling_ignored_during_execution(self, preferred_during_scheduling_ignored_during_execution):
         """
         Sets the preferred_during_scheduling_ignored_during_execution of this V1PodAffinity.
-        The scheduler will prefer to schedule pods to nodes that satisfy the affinity expressions specified by this field, but it may choose a node that violates one or more of the expressions. The node that is most preferred is the one with the greatest sum of weights, i.e. for each node that meets all of the scheduling requirements (resource request, requiredDuringScheduling affinity expressions, etc.), compute a sum by iterating through the elements of this field and adding \"weight\" to the sum if the node has pods which matches the corresponding podAffinityTerm; the node(s) with the highest sum are the most preferred.
+        The scheduler will prefer to schedule pods to nodes that satisfy the affinity expressions specified by this field, but it may choose a node that violates one or more of the expressions. The node that is most preferred is the one with the greatest sum of weights, i.e. for each node that meets all of the scheduling requirements (resource request, requiredDuringScheduling affinity expressions, etc.), compute a sum by iterating through the elements of this field and adding \"weight\" to the sum if the node has pods which match the corresponding podAffinityTerm; the node(s) with the highest sum are the most preferred.
 
         :param preferred_during_scheduling_ignored_during_execution: The preferred_during_scheduling_ignored_during_execution of this V1PodAffinity.
         :type: list[V1WeightedPodAffinityTerm]
