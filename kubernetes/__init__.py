f6555c015aa031148a302ac39728c770c2cff6b1
Haowei Cai <haoweic@google.com>
2019-07-29 16:06:54 -0700

    generated client change for 8.0.2 release

--- kubernetes/__init__.py
+++ kubernetes/__init__.py
@@ -14,7 +14,7 @@
 
 __project__ = 'kubernetes'
 # The version is auto-updated. Please do not edit.
-__version__ = "8.0.1"
+__version__ = "8.0.2"
 
 import kubernetes.client
 import kubernetes.config
