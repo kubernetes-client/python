67e1850f0e3c81420ff7c8240ac2d787b847b67d
prahlad <47686646+Prahladk09@users.noreply.github.com>
2019-10-16 15:40:49 +0530

    library

--- examples/deployment_create.py
+++ examples/deployment_create.py
@@ -12,10 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+#importing required library
 from os import path
-
 import yaml
-
 from kubernetes import client, config
 
 
