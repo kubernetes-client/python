dee078639b5e848db73232397087a81f1a336510
Haowei Cai <haoweic@google.com>
2019-10-29 09:33:46 -0700

    Add alias package kubernetes.client.apis with deprecation warning

---
+++ kubernetes/client/apis/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import
+import warnings
+
+# flake8: noqa
+
+# alias kubernetes.client.api package and print deprecation warning
+from kubernetes.client.api import *
+warnings.warn(
+    "The package kubernetes.client.apis is renamed and deprecated, use kubernetes.client.api instead (please note that the trailing s was removed).",
+    DeprecationWarning
+)
