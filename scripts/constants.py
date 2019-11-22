a53fbe64a9fe99cff4370fde4f2af7e24fe72260
Haowei Cai <haoweic@google.com>
2019-11-22 11:46:54 -0800

    Update version constants for 11.0.0b2 release

--- scripts/constants.py
+++ scripts/constants.py
@@ -18,7 +18,7 @@ import sys
 KUBERNETES_BRANCH = "release-1.15"
 
 # client version for packaging and releasing.
-CLIENT_VERSION = "11.0.0b1"
+CLIENT_VERSION = "11.0.0b2"
 
 # Name of the release package
 PACKAGE_NAME = "kubernetes"
