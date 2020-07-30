3e57e127578b89e4d27065934616f0224a2c56b8
Scott Lee <scottlee@fastmail.com>
2020-07-29 23:08:23 -0700

    Generate client and remove tests directory

--- kubernetes/client/__init__.py
+++ kubernetes/client/__init__.py
@@ -14,7 +14,7 @@
 
 from __future__ import absolute_import
 
-__version__ = "12.0.0-snapshot"
+__version__ = "12.0.0a1"
 
 # import apis into sdk package
 from kubernetes.client.api.admissionregistration_api import AdmissionregistrationApi
