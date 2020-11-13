78d816bd773c643cc8094045123bf6c1a8411884
Nabarun Pal <pal.nabarun95@gmail.com>
2020-11-13 18:11:41 +0530

    Generate client 17.14.0a1
    Signed-off-by: Nabarun Pal <pal.nabarun95@gmail.com>

--- kubernetes/client/configuration.py
+++ kubernetes/client/configuration.py
@@ -347,7 +347,7 @@ class Configuration(object):
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: release-1.17\n"\
-               "SDK Package Version: 17.0.0-snapshot".\
+               "SDK Package Version: 17.14.0a1".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self):
