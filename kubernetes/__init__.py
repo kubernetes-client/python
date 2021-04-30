273698b1c3fdec4f0dc773fe4cfc786607b490a5
Scott Lee <scottleehello@gmail.com>
2021-04-30 07:15:33 -0700

    Update release branch and release tags

--- kubernetes/__init__.py
+++ kubernetes/__init__.py
@@ -14,7 +14,7 @@
 
 __project__ = 'kubernetes'
 # The version is auto-updated. Please do not edit.
-__version__ = "17.14.0a1"
+__version__ = "17.17.0b1"
 
 import kubernetes.client
 import kubernetes.config
