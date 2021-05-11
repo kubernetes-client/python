57ad5d237e3e4640c6cba34e9ba0a017ced72b02
Prabha <58968096+steveprabha@users.noreply.github.com>
2021-05-11 14:13:27 +0530

    Update job_crud.py

--- examples/job_crud.py
+++ examples/job_crud.py
@@ -59,14 +59,14 @@ def create_job(api_instance, job):
     get_job_status(api_instance)
 
 
-
 def get_job_status(api_instance):
     job_completed = False
     while not job_completed:
         api_response = api_instance.read_namespaced_job_status(
             name=JOB_NAME,
             namespace="default")
-        if api_response.status.succeeded is not None or api_response.status.failed is not None:
+        if api_response.status.succeeded is not None or \
+                api_response.status.failed is not None:
             job_completed = True
         sleep(1)
         print("Job status='%s'" % str(api_response.status))
