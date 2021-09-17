2b20069ebc05283352fbdc95bbdca2b6133a4175
Ritesh Vadodaria <Ritesh.Vadodaria@sas.com>
2021-09-17 17:42:28 -0400

    Update horizontal_auto_scale.py
    Fixed review comments added create method.

--- examples/horizontal_auto_scale.py
+++ examples/horizontal_auto_scale.py
@@ -14,9 +14,9 @@
 
 """
 Script has functions helping:
-GET, UPDATE and DELETE for kind : HorizontalPodAutoscaler.
+GET, UPDATE and DELETE for kind :HorizontalPodAutoscaler.
 The below class has common functions for HPA referenced
-from core_v1_api.py with little tweaks.
+from core_v1_api.py with few tweaks.
 """
 
 from kubernetes import client, config
@@ -27,17 +27,84 @@ from kubernetes.client.exceptions import (
 
 
 import six
+import os
 
 
 class KubeExample():
-
-    def __init__(self, kube_config_file=None, namespace=None):
+    """
+        Helper class with functions allowing CREATE|GET|PATCH|DELETE operations for HorizontalPodAutoscaler # noqa: E501
+    """
+    def __init__(self, kube_config_file=None):
         """
+        Function initializing class with specific config file or it will be picked from
+        :param str kube_config_file : kube config file. If None assuming environment var KUBECONFIG is set. # noqa: E501
         """
         self.kube_config_file = kube_config_file
-        self.namespace = namespace
-        config.load_kube_config(kube_config_file)
+        if kube_config_file:
+            config.load_kube_config(kube_config_file)
+        else:
+            if os.environ.get('KUBECONFIG'):
+                config.load_kube_config()
+            else:
+                print("ERROR: Please set environment variable `KUBECONFIG`")
+                quit()
+
         self.api_client = client.CoreV1Api().api_client
+
+    def create_namespaced_hpa(self, namespace, body, **kwargs):
+        """create_namespaced_hpa
+        This method makes a synchronous POST HTTP request by default.
+        To make an asynchronous HTTP request, please pass async_req=True
+        >>> thread = api.create_namespaced_hpa(name, namespace, body, async_req=True)  # noqa: E501
+        >>> result = thread.get()
+        :param async_req bool: execute request asynchronously
+        :param str namespace: object name and auth scope, such as for teams and projects (required)
+        :param object body: (required)
+        :param str pretty: If 'true', then the output is pretty printed.
+        :param str dry_run: When present, indicates that modifications should not be persisted. An invalid or unrecognized dryRun directive will result in an error response and no further processing of the request. Valid values are: - All: all dry run stages will be processed
+        :param str field_manager: fieldManager is a name associated with the actor or entity that is making these changes. The value must be less than or 128 characters long, and only contain printable characters, as defined by https://golang.org/pkg/unicode/#IsPrint. This field is required for apply requests (application/apply-patch) but optional for non-apply patch types (JsonPatch, MergePatch, StrategicMergePatch).
+        :param bool force: Force is going to \"force\" Apply requests. It means user will re-acquire conflicting fields owned by other people. Force flag must be unset for non-apply patch requests.
+        :param _preload_content: if False, the urllib3.HTTPResponse object will
+                                 be returned without reading/decoding response
+                                 data. Default is False.
+        :param _request_timeout: timeout setting for this request. If one
+                                 number provided, it will be total request
+                                 timeout. It can also be a pair (tuple) of
+                                 (connection, read) timeouts.
+        :return: ***
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+        kwargs['_return_http_data_only'] = True
+
+        return self.hpa_namespaced_with_http_info('', namespace, body, _request_type='POST', _ignore_params=[], **kwargs)  # noqa: E501
+
+    def get_namespaced_hpa(self, name, namespace, **kwargs):  # noqa: E501
+        """get_namespaced_hpa # noqa: E501
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+        >>> thread = api.get_namespaced_hpa(name, namespace,  async_req=True)
+        >>> result = thread.get()
+        :param async_req bool: execute request asynchronously
+        :param str name: name of the hpa (required)
+        :param str namespace: object name and auth scope, such as for teams and projects (required)
+        :param str pretty: If 'true', then the output is pretty printed.
+        :param str dry_run: When present, indicates that modifications should not be persisted. An invalid or unrecognized dryRun directive will result in an error response and no further processing of the request. Valid values are: - All: all dry run stages will be processed
+        :param str field_manager: fieldManager is a name associated with the actor or entity that is making these changes. The value must be less than or 128 characters long, and only contain printable characters, as defined by https://golang.org/pkg/unicode/#IsPrint. This field is required for apply requests (application/apply-patch) but optional for non-apply patch types (JsonPatch, MergePatch, StrategicMergePatch).
+        :param bool force: Force is going to \"force\" Apply requests. It means user will re-acquire conflicting fields owned by other people. Force flag must be unset for non-apply patch requests.
+        :param _preload_content: if False, the urllib3.HTTPResponse object will
+                                 be returned without reading/decoding response
+                                 data. Default is False.
+        :param _request_timeout: timeout setting for this request. If one
+                                 number provided, it will be total request
+                                 timeout. It can also be a pair (tuple) of
+                                 (connection, read) timeouts.
+        :return: ***
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+        kwargs['_return_http_data_only'] = True
+        return self.hpa_namespaced_with_http_info(name=name, namespace=namespace, body=None, _request_type='GET', _ignore_params=['body'], **kwargs)  # noqa: E501
 
     def patch_namespaced_hpa(self, name, namespace, body, **kwargs):  # noqa: E501
         """patch_namespaced_hpa # noqa: E501
@@ -71,15 +138,12 @@ class KubeExample():
 
         return self.hpa_namespaced_with_http_info(name, namespace, body, _request_type='PATCH', _ignore_params=[], **kwargs)  # noqa: E501
 
-    def get_namespaced_hpa(self, name, namespace, **kwargs):  # noqa: E501
-        """get_namespaced_hpa # noqa: E501
-
-        partially update the specified hpa  # noqa: E501
+    def delete_namespaced_hpa(self, name, namespace, **kwargs):
+        """delete_namespaced_hpa # noqa: E501
         This method makes a synchronous HTTP request by default. To make an
         asynchronous HTTP request, please pass async_req=True
-        >>> thread = api.get_namespaced_hpa(name, namespace,  async_req=True)
+        >>> thread = api.delete_namespaced_hpa(name, namespace,  async_req=True)
         >>> result = thread.get()
-
         :param async_req bool: execute request asynchronously
         :param str name: name of the hpa (required)
         :param str namespace: object name and auth scope, such as for teams and projects (required)
@@ -99,51 +163,19 @@ class KubeExample():
                  returns the request thread.
         """
         kwargs['_return_http_data_only'] = True
-        return self.hpa_namespaced_with_http_info(name=name, namespace=namespace, body=None, _request_type='GET', _ignore_params=['body'])  # noqa: E501
-
-    def delete_namespaced_hpa(self, name, namespace, **kwargs):  # noqa: E501
-        """get_namespaced_hpa # noqa: E501
-
-        partially update the specified hpa  # noqa: E501
-        This method makes a synchronous HTTP request by default. To make an
-        asynchronous HTTP request, please pass async_req=True
-        >>> thread = api.get_namespaced_hpa(name, namespace,  async_req=True)
-        >>> result = thread.get()
-
-        :param async_req bool: execute request asynchronously
-        :param str name: name of the hpa (required)
-        :param str namespace: object name and auth scope, such as for teams and projects (required)
-        :param str pretty: If 'true', then the output is pretty printed.
-        :param str dry_run: When present, indicates that modifications should not be persisted. An invalid or unrecognized dryRun directive will result in an error response and no further processing of the request. Valid values are: - All: all dry run stages will be processed
-        :param str field_manager: fieldManager is a name associated with the actor or entity that is making these changes. The value must be less than or 128 characters long, and only contain printable characters, as defined by https://golang.org/pkg/unicode/#IsPrint. This field is required for apply requests (application/apply-patch) but optional for non-apply patch types (JsonPatch, MergePatch, StrategicMergePatch).
-        :param bool force: Force is going to \"force\" Apply requests. It means user will re-acquire conflicting fields owned by other people. Force flag must be unset for non-apply patch requests.
-        :param _preload_content: if False, the urllib3.HTTPResponse object will
-                                 be returned without reading/decoding response
-                                 data. Default is False.
-        :param _request_timeout: timeout setting for this request. If one
-                                 number provided, it will be total request
-                                 timeout. It can also be a pair (tuple) of
-                                 (connection, read) timeouts.
-        :return: ***
-                 If the method is called asynchronously,
-                 returns the request thread.
-        """
-        kwargs['_return_http_data_only'] = True
-        return self.hpa_namespaced_with_http_info(name=name, namespace=namespace, body=None, _request_type='DELETE', _ignore_params=['body'])  # noqa: E501
+        return self.hpa_namespaced_with_http_info(name=name, namespace=namespace, body=None, _request_type='DELETE', _ignore_params=['body'], **kwargs)  # noqa: E501
 
     def hpa_namespaced_with_http_info(self, name, namespace, body, **kwargs):  # noqa: E501
-        """patch_namespaced_hpa  # noqa: E501
-
-        partially update the specified hpa  # noqa: E501
+        """hpa_namespaced_with_http_info  # noqa: E501
+        This is a generic method we are using for all requests: CREATE|GET|PATCH|DELETE.
         This method makes a synchronous HTTP request by default. To make an
         asynchronous HTTP request, please pass async_req=True
-        >>> thread = api.hpa_namespaced_with_http_info(name, namespace, body, request_type='GET|PATCH',async_req=True)
+        >>> thread = api.hpa_namespaced_with_http_info(name, namespace, body, request_type='CREATE|GET|PATCH|DELETE',async_req=True)
         >>> result = thread.get()
-
         :param async_req bool: execute request asynchronously
         :param str name: name of the hpa (required)
         :param str namespace: object name and auth scope, such as for teams and projects (required)
-        :param object body: (required)
+        :param object body: (required for CREATE|PATCH)
         :param str pretty: If 'true', then the output is pretty printed.
         :param str dry_run: When present, indicates that modifications should not be persisted. An invalid or unrecognized dryRun directive will result in an error response and no further processing of the request. Valid values are: - All: all dry run stages will be processed
         :param str field_manager: fieldManager is a name associated with the actor or entity that is making these changes. The value must be less than or 128 characters long, and only contain printable characters, as defined by https://golang.org/pkg/unicode/#IsPrint. This field is required for apply requests (application/apply-patch) but optional for non-apply patch types (JsonPatch, MergePatch, StrategicMergePatch).
@@ -189,7 +221,7 @@ class KubeExample():
             if key not in all_params:
                 raise ApiTypeError(
                     "Got an unexpected keyword argument '%s'"
-                    " to method patch_namespaced_hpa" % key
+                    " to method hpa_namespaced_with_http_info" % key
                 )
             local_var_params[key] = val
 
@@ -197,16 +229,16 @@ class KubeExample():
         # verify the required parameter 'name' is set
         if self.api_client.client_side_validation and ('name' not in local_var_params or  # noqa: E501
                                                        local_var_params['name'] is None):  # noqa: E501
-            raise ApiValueError("Missing the required parameter `name` when calling `patch_namespaced_hpa`")  # noqa: E501
+            raise ApiValueError("Missing the required parameter `name` when calling `hpa_namespaced_with_http_info`")  # noqa: E501
         # verify the required parameter 'namespace' is set
         if self.api_client.client_side_validation and ('namespace' not in local_var_params or  # noqa: E501
                                                        local_var_params['namespace'] is None):  # noqa: E501
-            raise ApiValueError("Missing the required parameter `namespace` when calling `patch_namespaced_hpa`")  # noqa: E501
+            raise ApiValueError("Missing the required parameter `namespace` when calling `hpa_namespaced_with_http_info`")  # noqa: E501
 
         # verify the required parameter 'body' is set
         if self.api_client.client_side_validation and 'body' not in local_var_params['_ignore_params'] and ('body' not in local_var_params or  # noqa: E501
                                                                                                             local_var_params['body'] is None):  # noqa: E501
-            raise ApiValueError("Missing the required parameter `body` when calling `patch_namespaced_hpa`")  # noqa: E501
+            raise ApiValueError("Missing the required parameter `body` when calling `hpa_namespaced_with_http_info`")  # noqa: E501
 
         collection_formats = {}
 
@@ -236,18 +268,19 @@ class KubeExample():
             body_params = local_var_params['body']
 
         # HTTP header `Accept`
-
         header_params['Accept'] = self.api_client.select_header_accept(
             ['application/json', 'application/yaml', 'application/vnd.kubernetes.protobuf'])  # noqa: E501
 
-        # HTTP header `Content-Type`
+        content_list = ['application/json-patch+json', 'application/merge-patch+json', 'application/strategic-merge-patch+json', 'application/apply-patch+yaml']  # noqa: E501
 
-        header_params['Content-Type'] = self.api_client.select_header_content_type(  # noqa: E501
-            ['application/json-patch+json', 'application/merge-patch+json', 'application/strategic-merge-patch+json', 'application/apply-patch+yaml'])  # noqa: E501
+        if local_var_params['_request_type'] != 'PATCH':
+            content_list = content_list + ['application/json', 'application/yaml']  # noqa: E501
+
+        # HTTP header `Content-Type`
+        header_params['Content-Type'] = self.api_client.select_header_content_type(content_list)  # noqa: E501
 
         # Authentication setting
         auth_settings = ['BearerToken']  # noqa: E501
-        print(local_var_params['_request_type'])
 
         return self.api_client.call_api(
             '/apis/autoscaling/v1/namespaces/{namespace}/horizontalpodautoscalers/{name}', local_var_params['_request_type'],  # noqa: E501
@@ -267,29 +300,62 @@ class KubeExample():
 
 
 def main():
-    kubeEx = KubeExample(kube_config_file="<kube config file>", namespace="<namespace>")  # noqa: E501
-    namespace = "<namespace>"
-    hpa = "<hpa>"
+    kubeEx = KubeExample()
+    namespace = "default"
+    hpa = "test-hpa"
+    """
+    Create HPA example
+    """
+    body_dict = {"apiVersion": "autoscaling/v1",
+                 "kind": "HorizontalPodAutoscaler",
+                 "metadata": {
+                     "name": hpa,
+                     "namespace": namespace
+                             },
+                 "spec": {
+                   "scaleTargetRef": {
+                       "apiVersion": "apps/v1",
+                       "kind": "Deployment",
+                       "name": hpa
+                                     },
+                   "minReplicas": 1,
+                   "maxReplicas": 1,
+                   "targetCPUUtilizationPercentage": 80
+                         }
+                 }
+
+    resp_post = None
+    try:
+        resp_post = kubeEx.create_namespaced_hpa(namespace, body_dict)  # noqa: E501
+    except Exception as e:
+        resp_post = e
+
+    print("Create HPA Status:", resp_post.status)
 
     """
      Patching max and min replicas in Horizontal pod autoscaler.
     """
-    resp = kubeEx.patch_namespaced_hpa(hpa, namespace, {"spec": {"maxReplicas": 3, "minReplicas": 3}})  # noqa: E501
-    print(resp)
+    resp_patch = None
+    try:
+        resp_patch = kubeEx.patch_namespaced_hpa(hpa, namespace, {"spec": {"maxReplicas": 3, "minReplicas": 3}})  # noqa: E501
+    except Exception as e:
+        resp_patch = e
+
+    print("Patch HPA Status:", resp_patch.status)
 
     """
       Getting details of Horizontal pod autoscaler.
     """
-    resp_get, status, dict = kubeEx.get_namespaced_hpa(hpa, namespace)
+    resp_get = kubeEx.get_namespaced_hpa(hpa, namespace)
+    print("GET HPA Status:", resp_get.status)
     print(resp_get.read().decode())
 
     """Delete of Horizontal pod autoscaler.
     """
     resp_del = kubeEx.delete_namespaced_hpa(hpa, namespace)
-    print(resp_del)
+    print("DELETE HPA Status:", resp_del.status)
 
 
 if __name__ == '__main__':
-
     print("In main")
     main()
