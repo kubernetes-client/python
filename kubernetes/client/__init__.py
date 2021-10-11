9c829a736ef3d39802d1973a8f6c7669c782c57f
Haowei Cai <haoweic@google.com>
2021-10-10 19:04:45 -0700

    generated client change

--- kubernetes/client/__init__.py
+++ kubernetes/client/__init__.py
@@ -14,7 +14,7 @@
 
 from __future__ import absolute_import
 
-__version__ = "19.15.0a1"
+__version__ = "19.15.0b1"
 
 # import apis into sdk package
 from kubernetes.client.api.admissionregistration_api import AdmissionregistrationApi
--- kubernetes/client/configuration.py
+++ kubernetes/client/configuration.py
@@ -347,7 +347,7 @@ class Configuration(object):
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: release-1.19\n"\
-               "SDK Package Version: 19.15.0a1".\
+               "SDK Package Version: 19.15.0b1".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self):
