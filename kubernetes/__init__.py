6d785aabd544e45aa6fd5a782b4c0f4ee5a18430
Yu Liao <yliao@google.com>
2021-10-25 14:41:05 -0700

    generated client change

--- kubernetes/__init__.py
+++ kubernetes/__init__.py
@@ -14,7 +14,7 @@
 
 __project__ = 'kubernetes'
 # The version is auto-updated. Please do not edit.
-__version__ = "19.0.0-snapshot"
+__version__ = "20.0.0-snapshot"
 
 import kubernetes.client
 import kubernetes.config
