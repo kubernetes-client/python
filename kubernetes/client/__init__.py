16e8ddcef4bc7bd88cbc787787f617d5df34789a
Yu Liao <yliao@google.com>
2021-11-19 09:35:17 -0800

    generated client change

--- kubernetes/client/__init__.py
+++ kubernetes/client/__init__.py
@@ -14,7 +14,7 @@
 
 from __future__ import absolute_import
 
-__version__ = "20.12.0b1"
+__version__ = "20.13.0"
 
 # import apis into sdk package
 from kubernetes.client.api.well_known_api import WellKnownApi
--- kubernetes/client/configuration.py
+++ kubernetes/client/configuration.py
@@ -350,7 +350,7 @@ class Configuration(object):
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: release-1.20\n"\
-               "SDK Package Version: 20.12.0b1".\
+               "SDK Package Version: 20.13.0".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self):
