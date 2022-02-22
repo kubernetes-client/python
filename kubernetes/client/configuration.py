7ced348aa3e9db515b0d9bf808239341999e8fc6
Yu Liao <yliao@google.com>
2022-02-22 09:12:01 -0800

    generated client change

--- kubernetes/client/configuration.py
+++ kubernetes/client/configuration.py
@@ -350,7 +350,7 @@ class Configuration(object):
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: release-1.23\n"\
-               "SDK Package Version: 23.3.0a1".\
+               "SDK Package Version: 23.3.0b1".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self):
