35d3ff0216834141ffd73fc693d81a6a90058363
Ron <ronnymallory@gmail.com>
2022-03-16 18:13:54 -0700

    docs(examples): add example to utilize utils

---
+++ examples/apply_from_directory.py
@@ -0,0 +1,10 @@
+from kubernetes import client,config,utils
+
+def main():
+    config.load_kube_config()
+    k8s_client = client.ApiClient()
+    yaml_dir = 'examples/yaml_dir/'
+    utils.create_from_directory(k8s_client, yaml_dir,verbose=True)
+
+if __name__ == "__main__":
+    main()
