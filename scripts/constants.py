c7b928c260fc9939bcb6e38bd94fdb190e82fcc9
Yu Liao <yliao@google.com>
2022-06-06 21:53:18 +0000

    update version constants for 24.1.0a1 release

--- scripts/constants.py
+++ scripts/constants.py
@@ -18,7 +18,7 @@ import sys
 KUBERNETES_BRANCH = "release-1.24"
 
 # client version for packaging and releasing.
-CLIENT_VERSION = "24.0.0-snapshot"
+CLIENT_VERSION = "24.1.0a1"
 
 # Name of the release package
 PACKAGE_NAME = "kubernetes"
