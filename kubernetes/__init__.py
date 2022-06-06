4bf4538dea91e3df9977c118589d4abd25c67d88
Yu Liao <yliao@google.com>
2022-06-06 17:32:42 +0000

    generated client change

--- kubernetes/__init__.py
+++ kubernetes/__init__.py
@@ -14,7 +14,7 @@
 
 __project__ = 'kubernetes'
 # The version is auto-updated. Please do not edit.
-__version__ = "23.0.1-snapshot"
+__version__ = "24.1.0"
 
 import kubernetes.client
 import kubernetes.config
