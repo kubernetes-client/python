6e8b82b314d66bbf784826955ee04f878610afa7
Yu Liao <yliao@google.com>
2022-06-13 16:49:22 +0000

    update version constants for 24.1.0b1 release

--- scripts/constants.py
+++ scripts/constants.py
@@ -18,13 +18,13 @@ import sys
 KUBERNETES_BRANCH = "release-1.24"
 
 # client version for packaging and releasing.
-CLIENT_VERSION = "24.1.0a1"
+CLIENT_VERSION = "24.1.0b1"
 
 # Name of the release package
 PACKAGE_NAME = "kubernetes"
 
 # Stage of development, mainly used in setup.py's classifiers.
-DEVELOPMENT_STATUS = "3 - Alpha"
+DEVELOPMENT_STATUS = "4 - Beta"
 
 
 # If called directly, return the constant value given
