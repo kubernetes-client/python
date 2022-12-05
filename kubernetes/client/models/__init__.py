6cede9e2ccfb524dec88bd3880ad939918a7e98b
dsaimukund <dsai.mukund@unacademy.com>
2022-12-05 12:02:00 +0530

    add in init

--- kubernetes/client/models/__init__.py
+++ kubernetes/client/models/__init__.py
@@ -369,6 +369,7 @@ from .v1_token_review_status import V1TokenReviewStatus
 from .v1_toleration import V1Toleration
 from .v1_topology_selector_label_requirement import V1TopologySelectorLabelRequirement
 from .v1_topology_selector_term import V1TopologySelectorTerm
+from .v1_topology_spread_constraint import V1TopologySpreadConstraint
 from .v1_typed_local_object_reference import V1TypedLocalObjectReference
 from .v1_user_info import V1UserInfo
 from .v1_volume import V1Volume
