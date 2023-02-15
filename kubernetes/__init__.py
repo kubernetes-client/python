a688be31b8919b70d233d41b44ef5344e809bca7
Yu Liao <yliao@google.com>
2023-02-15 06:03:45 +0000

    generated client change

--- kubernetes/__init__.py
+++ kubernetes/__init__.py
@@ -14,7 +14,7 @@
 
 __project__ = 'kubernetes'
 # The version is auto-updated. Please do not edit.
-__version__ = "26.1.0b1"
+__version__ = "26.1.0"
 
 from . import client
 from . import config
--- kubernetes/client/configuration.py
+++ kubernetes/client/configuration.py
@@ -350,7 +350,7 @@ class Configuration(object):
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: release-1.26\n"\
-               "SDK Package Version: 26.1.0b1".\
+               "SDK Package Version: 26.1.0".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self):
