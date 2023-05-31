42d6156b27879f3ede8050b0f19c33c6f317d297
Yu Liao <yliao@google.com>
2023-05-31 21:09:36 +0000

    generated client change

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import setup
 
 # Do not edit these constants. They will be updated automatically
 # by scripts/update-client.sh.
-CLIENT_VERSION = "26.0.0+snapshot"
+CLIENT_VERSION = "27.0.0+snapshot"
 PACKAGE_NAME = "kubernetes"
 DEVELOPMENT_STATUS = "3 - Alpha"
 
