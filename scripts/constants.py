db2908a7ec1d9e6645f5727357e885260c946db8
Yu Liao <yliao@google.com>
2023-05-31 21:09:14 +0000

    update version constants for 27.0.0+snapshot release

--- scripts/constants.py
+++ scripts/constants.py
@@ -15,10 +15,10 @@
 import sys
 
 # Kubernetes branch to get the OpenAPI spec from.
-KUBERNETES_BRANCH = "release-1.26"
+KUBERNETES_BRANCH = "release-1.27"
 
 # client version for packaging and releasing.
-CLIENT_VERSION = "26.0.0+snapshot"
+CLIENT_VERSION = "27.0.0+snapshot"
 
 # Name of the release package
 PACKAGE_NAME = "kubernetes"
