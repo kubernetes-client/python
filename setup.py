a228d4312215b1b5f95a23d2982ebc2e3fcec5cf
Rayyan Baig <137826567+Rayyanbaig1243@users.noreply.github.com>
2023-08-05 14:17:05 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,32 +1,31 @@
-# Copyright 2016 The Kubernetes Authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+Copyright 2016 The Kubernetes Authors.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
 
 from setuptools import setup
 
-# Do not edit these constants. They will be updated automatically
-# by scripts/update-client.sh.
+Do not edit these constants. They will be updated automatically
+ by scripts/update-client.sh.
 CLIENT_VERSION = "27.0.0+snapshot"
 PACKAGE_NAME = "kubernetes"
 DEVELOPMENT_STATUS = "3 - Alpha"
 
-# To install the library, run the following
-#
-# python setup.py install
-#
-# prerequisite: setuptools
-# http://pypi.python.org/pypi/setuptools
+ To install the library, run the following
 
+ python setup.py install
+
+ prerequisite: setuptools
+ http://pypi.python.org/pypi/setuptools
 EXTRAS = {
     'adal': ['adal>=1.0.2']
 }
@@ -69,19 +68,19 @@ setup(
     long_description="Python client for kubernetes http://kubernetes.io/",
     python_requires='>=3.6',
     classifiers=[
-        "Development Status :: %s" % DEVELOPMENT_STATUS,
-        "Topic :: Utilities",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Information Technology",
-        "License :: OSI Approved :: Apache Software License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
+        Development Status :: %s" % DEVELOPMENT_STATUS,
+        Topic :: Utilities",
+        Intended Audience :: Developers",
+        Intended Audience :: Information Technology",
+        License :: OSI Approved :: Apache Software License",
+        Operating System :: OS Independent",
+        Programming Language :: Python",
+        Programming Language :: Python :: 3",
+        Programming Language :: Python :: 3.6",
+        Programming Language :: Python :: 3.7",
+        Programming Language :: Python :: 3.8",
+        Programming Language :: Python :: 3.9",
+        Programming Language :: Python :: 3.10",
+Programming Language :: Python :: 3.11",
     ],
 )
