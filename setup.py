0118f24c525c1ae566dc2fb19ddd2c1a0de2127e
Rayyan Baig <137826567+Rayyanbaig1243@users.noreply.github.com>
2023-08-07 14:15:41 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,78 +1,68 @@
 Copyright 2016 The Kubernetes Authors.
-
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
  You may obtain a copy of the License at
-
-     http://www.apache.org/licenses/LICENSE-2.0
-
+ http://www.apache.org/licenses/LICENSE-2.0
  Unless required by applicable law or agreed to in writing, software
  distributed under the License is distributed on an "AS IS" BASIS,
  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  See the License for the specific language governing permissions and
  limitations under the License.
-
-from setuptools import setup
-
-Do not edit these constants. They will be updated automatically
+ from setuptools import setup
+ Do not edit these constants. They will be updated automatically
  by scripts/update-client.sh.
-CLIENT_VERSION = "27.0.0+snapshot"
-PACKAGE_NAME = "kubernetes"
-DEVELOPMENT_STATUS = "3 - Alpha"
-
+ CLIENT_VERSION = "27.0.0+snapshot"
+ PACKAGE_NAME = "kubernetes"
+ DEVELOPMENT_STATUS = "3 - Alpha"
  To install the library, run the following
-
  python setup.py install
-
  prerequisite: setuptools
  http://pypi.python.org/pypi/setuptools
-EXTRAS = {
+ EXTRAS = {
     'adal': ['adal>=1.0.2']
 }
-REQUIRES = []
-with open('requirements.txt') as f:
-    for line in f:
-        line, _, _ = line.partition('#')
-        line = line.strip()
-        if not line or line.startswith('setuptools'):
-            continue
-        elif ';' in line:
-            requirement, _, specifier = line.partition(';')
-            for_specifier = EXTRAS.setdefault(':{}'.format(specifier), [])
-            for_specifier.append(requirement)
-        else:
-            REQUIRES.append(line)
-
-with open('test-requirements.txt') as f:
+ REQUIRES = []
+ with open('requirements.txt') as f:
+ for line in f:
+ line, _, _ = line.partition('#')
+ line = line.strip()
+ if not line or line.startswith('setuptools'):
+ continue
+ elif ';' in line:
+ requirement, _, specifier = line.partition(';')
+ for_specifier = EXTRAS.setdefault(':{}'.format(specifier), [])
+ for_specifier.append(requirement)
+ else:
+ REQUIRES.append(line)
+ with open('test-requirements.txt') as f:
     TESTS_REQUIRES = f.readlines()
-
 setup(
     name=PACKAGE_NAME,
     version=CLIENT_VERSION,
-    description="Kubernetes python client",
-    author_email="",
-    author="Kubernetes",
-    license="Apache License Version 2.0",
-    url="https://github.com/kubernetes-client/python",
-    keywords=["Swagger", "OpenAPI", "Kubernetes"],
+    description=Kubernetes python client,
+    author_email=,
+    author=Kubernetes,
+    license=Apache License Version 2.0,
+    url=https://github.com/kubernetes-client/python,
+    keywords=[Swagger, OpenAPI, Kubernetes],
     install_requires=REQUIRES,
     tests_require=TESTS_REQUIRES,
-    extras_require=EXTRAS,
-    packages=['kubernetes', 'kubernetes.client', 'kubernetes.config',
-              'kubernetes.watch', 'kubernetes.client.api',
-              'kubernetes.stream', 'kubernetes.client.models',
-              'kubernetes.utils', 'kubernetes.client.apis',
-              'kubernetes.dynamic', 'kubernetes.leaderelection',
-              'kubernetes.leaderelection.resourcelock'],
+    extras_require=EXTRAS
+    packages=[kubernetes, kubernetes.client, kubernetes.config,
+              kubernetes.watch, kubernetes.client.api,
+              kubernetes.stream, kubernetes.client.models,
+              kubernetes.utils, kubernetes.client.apis,
+              kubernetes.dynamic, kubernetes.leaderelection,
+              kubernetes.leaderelection.resourcelock],
     include_package_data=True,
-    long_description="Python client for kubernetes http://kubernetes.io/",
-    python_requires='>=3.6',
+    long_description=Python client for kubernetes http://kubernetes.io/,
+    python_requires=>=3.6,
     classifiers=[
         Development Status :: %s" % DEVELOPMENT_STATUS,
         Topic :: Utilities",
-        Intended Audience :: Developers",
-        Intended Audience :: Information Technology",
-        License :: OSI Approved :: Apache Software License",
+        Intended Audience :: Developers,
+        Intended Audience :: Information Technology,
+        License :: OSI Approved :: Apache Software License,
         Operating System :: OS Independent",
         Programming Language :: Python",
         Programming Language :: Python :: 3",
@@ -81,6 +71,6 @@ setup(
         Programming Language :: Python :: 3.8",
         Programming Language :: Python :: 3.9",
         Programming Language :: Python :: 3.10",
-Programming Language :: Python :: 3.11",
+        Programming Language :: Python :: 3.11",
     ],
 )
