d77daf461c48d8d3307d83894562cb96971ab913
Ananya Nayak <ananyanayak102@gmail.com>
2023-09-11 19:10:57 +0530

    chore: move yaml files to correct folder

--- examples/deployment_create.py
+++ examples/deployment_create.py
@@ -29,7 +29,7 @@ def main():
     # default location.
     config.load_kube_config()
 
-    with open(path.join(path.dirname(__file__), "nginx-deployment.yaml")) as f:
+    with open(path.join(path.dirname(__file__), "yaml_dir/nginx-deployment.yaml")) as f:
         dep = yaml.safe_load(f)
         k8s_apps_v1 = client.AppsV1Api()
         resp = k8s_apps_v1.create_namespaced_deployment(
