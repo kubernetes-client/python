9232207c1e6348f0ff48f812cb243c9f26dfa5e0
Bhavana5N <34437924+Bhavana5N@users.noreply.github.com>
2024-04-11 15:29:20 -0400

    test
    test

--- examples/apply_from_single_file.py
+++ examples/apply_from_single_file.py
@@ -5,6 +5,7 @@ def main():
     k8s_client = client.ApiClient()
     yaml_file = 'examples/yaml_dir/configmap-demo-pod.yml'
     utils.create_from_yaml(k8s_client,yaml_file,verbose=True)
+    print("testpassword")
 
 if __name__ == "__main__":
     main()
