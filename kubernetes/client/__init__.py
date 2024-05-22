44d934e6b7f02aa7b289eb06a7f04c9231ffcc73
yliao <yliao@google.com>
2024-05-22 00:32:35 +0000

    generated client change

--- kubernetes/client/__init__.py
+++ kubernetes/client/__init__.py
@@ -14,7 +14,7 @@
 
 from __future__ import absolute_import
 
-__version__ = "30.0.0+snapshot"
+__version__ = "30.1.0a1"
 
 # import apis into sdk package
 from kubernetes.client.api.well_known_api import WellKnownApi
