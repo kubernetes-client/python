f05c642b4aa580be4613b2e4bb6ff35a3d6f0237
Flynn <emissary@flynn.kodachi.com>
2024-08-21 12:19:30 -0400

    Review feedback
    Signed-off-by: Flynn <emissary@flynn.kodachi.com>

--- examples/duration-gep2257.py
+++ examples/duration-gep2257.py
@@ -7,19 +7,19 @@ a GEP-2257 duration string (you can find the full specification at
 https://gateway-api.sigs.k8s.io/geps/gep-2257/).
 
 Good things to try:
->>> python examples/gep2257.py 1h
+>>> python examples/duration-gep2257.py 1h
 Duration: 1h
->>> python examples/gep2257.py 3600s
+>>> python examples/duration-gep2257.py 3600s
 Duration: 1h
->>> python examples/gep2257.py 90m
+>>> python examples/duration-gep2257.py 90m
 Duration: 1h30m
->>> python examples/gep2257.py 30m1h10s5s
+>>> python examples/duration-gep2257.py 30m1h10s5s
 Duration: 1h30m15s
->>> python examples/gep2257.py 0h0m0s0ms
+>>> python examples/duration-gep2257.py 0h0m0s0ms
 Duration: 0s
->>> python examples/gep2257.py -5m
+>>> python examples/duration-gep2257.py -5m
 ValueError: Invalid duration format: -5m
->>> python examples/gep2257.py 1.5h
+>>> python examples/duration-gep2257.py 1.5h
 ValueError: Invalid duration format: 1.5h
 """
 
