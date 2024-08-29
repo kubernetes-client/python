fbda28111659a37d6c4ee28278e0fdf4d5b8416c
yliao <yliao@google.com>
2024-08-29 22:50:36 +0000

    update version constants for 31.0.0a1 release

--- scripts/constants.py
+++ scripts/constants.py
@@ -18,7 +18,7 @@ import sys
 KUBERNETES_BRANCH = "release-1.31"
 
 # client version for packaging and releasing.
-CLIENT_VERSION = "31.0.0+snapshot"
+CLIENT_VERSION = "31.0.0a1"
 
 # Name of the release package
 PACKAGE_NAME = "kubernetes"
