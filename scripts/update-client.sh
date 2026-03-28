3061ad2bd3ef8bbcd6df41d3c3e6e243bbbf6278
yliao <yliao@google.com>
2026-03-28 22:18:27 +0000

    removed api_client_dict_syntax.diff, openapi generator v6.6.0 already supports dict syntax

--- scripts/update-client.sh
+++ scripts/update-client.sh
@@ -69,12 +69,6 @@ sed -i'' "s,^DEVELOPMENT_STATUS = .*,DEVELOPMENT_STATUS = \\\"${DEVELOPMENT_STAT
 # second, this should be ported to swagger-codegen
 echo ">>> patching client..."
 git apply "${SCRIPT_ROOT}/rest_client_patch.diff"
-# Support dict[str, str] syntax in ApiClient deserializer alongside legacy
-# dict(str, str). This enables forward compatibility with modern Python typing
-# syntax while maintaining backward compatibility. Users can now convert
-# openapi_types for Pydantic integration.
-# See https://github.com/kubernetes-client/python/issues/2463
-git apply "${SCRIPT_ROOT}/api_client_dict_syntax.diff"
 # The following is commented out due to:
 # AttributeError: 'RESTResponse' object has no attribute 'headers'
 # OpenAPI client generator prior to 6.4.0 uses deprecated urllib3 APIs.
