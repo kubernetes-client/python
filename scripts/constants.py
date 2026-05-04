f3729763c66582ce801caa2cc1ded9bfd8806e61
yliao <yliao@google.com>
2026-05-04 16:22:49 +0000

    update version constants for 36.0.0+snapshot release

--- scripts/constants.py
+++ scripts/constants.py
@@ -15,10 +15,10 @@
 import sys
 
 # Kubernetes branch to get the OpenAPI spec from.
-KUBERNETES_BRANCH = "release-1.35"
+KUBERNETES_BRANCH = "release-1.36"
 
 # client version for packaging and releasing.
-CLIENT_VERSION = "35.0.0+snapshot"
+CLIENT_VERSION = "36.0.0+snapshot"
 
 # Name of the release package
 PACKAGE_NAME = "kubernetes"
