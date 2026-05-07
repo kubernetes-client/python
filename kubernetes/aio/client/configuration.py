049a4906b0b54a9057849e4073fa3108ee597963
yliao <yliao@google.com>
2026-05-07 21:59:04 +0000

    generated client change

--- kubernetes/aio/client/configuration.py
+++ kubernetes/aio/client/configuration.py
@@ -435,7 +435,7 @@ conf = client.Configuration(
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: release-1.36\n"\
-               "SDK Package Version: 36.0.0a2".\
+               "SDK Package Version: 36.0.0a3".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self):
--- kubernetes/client/__init__.py
+++ kubernetes/client/__init__.py
@@ -14,7 +14,7 @@
 
 from __future__ import absolute_import
 
-__version__ = "36.0.0a2"
+__version__ = "36.0.0a3"
 
 # import apis into sdk package
 from kubernetes.client.api.well_known_api import WellKnownApi
