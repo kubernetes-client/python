449e548c3f168ac146f774ff0ff72fe71cbd8dbb
yliao <yliao@google.com>
2026-05-13 19:55:22 +0000

    generated client change

--- kubernetes/aio/client/api_client.py
+++ kubernetes/aio/client/api_client.py
@@ -78,7 +78,7 @@ class ApiClient(object):
             self.default_headers[header_name] = header_value
         self.cookie = cookie
         # Set default User-Agent.
-        self.user_agent = 'OpenAPI-Generator/36.0.0a3/python'
+        self.user_agent = 'OpenAPI-Generator/36.0.0b1/python'
         self.client_side_validation = configuration.client_side_validation
 
     async def __aenter__(self):
